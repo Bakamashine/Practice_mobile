27906f6f72667e57a14b48ffdba4fa3f6b36270a
Bakamashine <ivan.fa.002@gmail.com>
2025-04-10 13:51:06 +0300

    update DroidQuest and README

--- app/DroidQuest/droidquest.service.ts
+++ app/DroidQuest/droidquest.service.ts
@@ -4,9 +4,3 @@ export function fabric(question: string, answer: boolean) {
         answer
     }
 }
-
-// export const onequestion = fabric("ОС Android основана на ядре Linux?", true)
-// export const twoquestion = fabric("Java является основным языком программирования для разработки приложения на Android?", false)
-// export const threequestion = fabric("APK - это формат файла для установки приложений на Android?", true)
-// export const fourquestion = fabric("Google Play Store - это магазин приложений для Android?", true)
-// export const fivequestion = fabric("Android поддерживает многозадачность?", true)
--- app/DroidQuest/index.tsx
+++ app/DroidQuest/index.tsx
@@ -1,60 +1,102 @@
-import React, { useState } from 'react';
-import { View, Text, TouchableOpacity, StyleSheet, Alert, ScrollView } from 'react-native';
-import { fabric } from './droidquest.service';
-
+import React, { useState } from "react";
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  StyleSheet,
+  Alert,
+  ScrollView,
+} from "react-native";
+import { fabric } from "./droidquest.service";
 
 const questions = [
   fabric("Android является операционной системой?", true),
-  fabric("Java является основным языком программирования для разработки приложений на Android?", false),
+  fabric(
+    "Java является основным языком программирования для разработки приложений на Android?",
+    false
+  ),
   fabric("APK - это формат файла для установки приложений на Android?", true),
   fabric("Google Play Store - это магазин приложений для Android?", true),
-  fabric("Android поддерживает многозадачность?", true)
-]
+  fabric("Android поддерживает многозадачность?", true),
+];
 
 const App = () => {
   const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0); // index
   const [score, setScore] = useState(0); // result
-  const [quizFinished, setQuizFinished] = useState(false); // переменная для проверки правильности ответа
+  const [quizFinished, setQuizFinished] = useState(false); // переменная для проверки решённого теста
+  const [status, setStatus] = useState(false);
 
   const handleAnswer = (userAnswer: boolean) => {
     if (userAnswer === questions[currentQuestionIndex].answer) {
+      setStatus(true);
       setScore(score + 1);
+    } else {
+      setStatus(false)
     }
-
+    
     if (currentQuestionIndex < questions.length - 1) {
       setCurrentQuestionIndex(currentQuestionIndex + 1);
     } else {
       setQuizFinished(true);
-      // Alert.alert("Тест завершен", `Ваш результат: ${score + (userAnswer === questions[currentQuestionIndex].answer ? 1 : 0)} из ${questions.length}`);
     }
   };
 
   if (quizFinished) {
     return (
       <View style={styles.container}>
-        <Text style={styles.resultText}>Ваш результат: {score} из {questions.length}</Text>
+        <Text style={styles.resultText}>
+          Ваш результат: {score} из {questions.length}
+        </Text>
+        <TouchableOpacity
+          style={styles.button}
+          onPress={() => {
+            setCurrentQuestionIndex(0);
+            setQuizFinished(false);
+            setScore(0);
+          }}
+        >
+          <Text>Пройти снова</Text>
+        </TouchableOpacity>
       </View>
     );
   }
 
   return (
     <ScrollView contentContainerStyle={styles.container}>
-      <Text style={styles.questionText}>{questions[currentQuestionIndex].question}</Text>
-      <TouchableOpacity style={styles.button} onPress={() => handleAnswer(true)}>
+      <Text style={styles.questionText}>
+        {questions[currentQuestionIndex].question}
+      </Text>
+      <TouchableOpacity
+        style={styles.button}
+        onPress={() => handleAnswer(true)}
+      >
         <Text>Да</Text>
       </TouchableOpacity>
-      <TouchableOpacity style={styles.button} onPress={() => handleAnswer(false)}>
+      <TouchableOpacity
+        style={styles.button}
+        onPress={() => handleAnswer(false)}
+      >
         <Text>Нет</Text>
       </TouchableOpacity>
-      
+
       {currentQuestionIndex > 0 ? (
-      <TouchableOpacity style={styles.button} onPress={() => {
-        setCurrentQuestionIndex(currentQuestionIndex-1);
-      }}>
-        <Text>Назад</Text>
-      </TouchableOpacity>
-) : <Text></Text>}
-      
+        <TouchableOpacity
+          style={styles.button}
+          onPress={() => {
+            setCurrentQuestionIndex(currentQuestionIndex - 1);
+
+            if (status) {
+              setScore(score - 1);
+            }
+          }}
+        >
+          <Text>Назад</Text>
+        </TouchableOpacity>
+      ) : (
+        <Text></Text>
+      )}
+      {/* <Text>Ваш результат: {score}</Text>
+      <Text>Статус ответа: {status.toString()}</Text> */}
     </ScrollView>
   );
 };
@@ -62,22 +104,22 @@ const App = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+    justifyContent: "center",
+    alignItems: "center",
     padding: 20,
-    backgroundColor: '#fff',
+    backgroundColor: "#fff",
   },
   questionText: {
     fontSize: 18,
     marginBottom: 20,
-    textAlign: 'center',
+    textAlign: "center",
   },
   button: {
-    backgroundColor: 'gray',
+    backgroundColor: "gray",
     padding: 10,
     marginVertical: 10,
-    width: '20%',
-    alignItems: 'center',
+    width: "40%",
+    alignItems: "center",
     borderRadius: 5,
   },
   resultText: {
