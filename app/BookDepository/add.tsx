186bf7bbf71df868a3e1a812c707be93d368e76a
Bakamashine <ivan.fa.002@gmail.com>
2025-04-16 14:33:54 +0300

    Небольшие обновления

--- app/BookDepository/add.tsx
+++ app/BookDepository/add.tsx
@@ -1,19 +1,40 @@
-import { View, TextInput, Text, TouchableOpacity } from "react-native";
+import React from "react";
+import { View, TextInput, Text, TouchableOpacity, StyleSheet} from "react-native";
+import BouncyCheckbox from "react-native-bouncy-checkbox";
 import styles from "@/components/BookDepository/styles";
 function add() {
+  /**
+   * Статус нажатия на checkbox
+   * @default false
+   */
+  const [check, setCheck] = React.useState(false);
+
   return (
     <View>
       <TextInput
         style={styles.TextInput}
         placeholder="Введите название книги."
       ></TextInput>
-      <View style={styles.center}>
+      <View style={[styles.center, add_style.flex]}>
         <TouchableOpacity style={styles.button}>
-          <Text>Добавить книгу</Text>
+          <Text>{Date()}</Text>
         </TouchableOpacity>
+        <BouncyCheckbox
+          textStyle={{ textDecorationLine: "none" }}
+          onPress={(isChecked: boolean) => setCheck(isChecked)}
+          size={25}
+          text="Прочтена?"
+        />
       </View>
     </View>
   );
 }
 
 export default add;
+
+const add_style = StyleSheet.create({
+  flex: {
+    flex: 1,
+
+  }
+})
