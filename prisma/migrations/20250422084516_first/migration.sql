2561ad15c0f0ec2d9170a05b69217432a9f2aacc
Bakamashine <ivan.fa.002@gmail.com>
2025-04-22 11:46:52 +0300

    Заработало

---
+++ prisma/migrations/20250422084516_first/migration.sql
@@ -0,0 +1,7 @@
+-- CreateTable
+CREATE TABLE "Books" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "title" TEXT NOT NULL,
+    "date" TEXT NOT NULL,
+    "readed" BOOLEAN NOT NULL
+);
